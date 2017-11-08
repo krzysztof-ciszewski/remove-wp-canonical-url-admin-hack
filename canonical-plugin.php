871b0c56bb23a3d6e7d9dc203bd73179eeb3671f
Krzysztof Ciszewski <krzysztof.ciszewski@xsolve.pl>
2017-11-08 12:41:18 +0100

    composer package

--- canonical-plugin.php
+++ canonical-plugin.php
@@ -1,5 +1,5 @@
 <?php
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined('ABSPATH') or die();
 /*
 	Plugin Name: Remove WP Canonical URL Admin Hack
 	Description: WordPress plugin to remove canonical URLs from Admin Area.  useful for Reversed Proxie/Multi Site Installations.  Stops the wp_admin_canonical_url redirect in WP-Admin after 4.2 upgrade 
@@ -11,13 +11,13 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
     License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
+global $wpdb;
+require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+// Adding action to call my_plugin_init on plugin initialisation
+add_action('init', 'wp_canonical_hack_init');
 
-    global $wpdb;
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    // Adding action to call my_plugin_init on plugin initialisation
-    add_action('init', 'wp_canonical_hack_init');
-
-    // Function to be called on plugin initialisation
-    function wp_canonical_hack_init() {
-        remove_action( 'admin_head', 'wp_admin_canonical_url' );
-    }
+// Function to be called on plugin initialisation
+function wp_canonical_hack_init()
+{
+    remove_action('admin_head', 'wp_admin_canonical_url');
+}
